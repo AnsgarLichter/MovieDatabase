ceb5452e5d90091e3c816baf21db6a7f80444fa9
linxside <git@linux-inside.de>
2022-12-02 17:20:31 +0100

    bug: fix tv show service

--- src/app/services/tv-show.service.ts
+++ src/app/services/tv-show.service.ts
@@ -1,24 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 import {map, Observable} from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { TvShow } from '../models/tv-show.model';
+import {environment} from 'src/environments/environment';
+import {TvShow} from '../models/tv-show.model';
 import {TvShowAdapter} from "../adapters/tv-show.adapter";
 import {TMDB_TvShow} from "../models/tmdb/tmdb-tv-show-model";
 
 @Injectable({
   providedIn: 'root'
 })
-export class TVShowService{
+export class TVShowService {
   private apiServerUrl = environment.movieDbApi;
   private apiKey = environment.movieDbApiKey;
   private apiVersion = environment.movieDbApiVersion;
 
-  protected getBaselineUrl(): String {
+  protected getBaselineUrl(): string {
     return `${this.apiServerUrl}/${this.apiVersion}`
   }
 
-  protected getQueryParameterForApiKey(): String {
+  protected getQueryParameterForApiKey(): string {
     return `api_key=${this.apiKey}`
   }
 
@@ -26,10 +26,19 @@ export class TVShowService{
     this.adapter = adapter;
   }
 
-  public getTVShow(tvShowID: number): Observable<TvShow>{
+  public getTVShow(tvShowID: number): Observable<TvShow> {
     let requestUrl = `${this.getBaselineUrl()}/tv/${tvShowID}?${this.getQueryParameterForApiKey()}`;
 
-    return this.httpClient.get<TMDB_TvShow>(requestUrl).pipe(
+    const appendToResponse: string[] = ["&append_to_response="];
+    appendToResponse.push(`credits`);
+    appendToResponse.push(`watch/providers`);
+    appendToResponse.push(`keywords`);
+
+    let parameters = appendToResponse.join(",")
+
+    console.log(requestUrl + parameters)
+
+    return this.httpClient.get<TMDB_TvShow>(requestUrl + parameters).pipe(
       map((tvShow: TMDB_TvShow) => this.adapter.adapt(tvShow))
     );
   }
