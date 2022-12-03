4f104fcdef60be4fb5cf4310f607da7c3677c62e
Ansgar Lichter <lichteransgar@gmail.com>
2022-12-03 18:27:01 +0100

    refactor: extract mapping in methods

--- src/app/adapters/watch-provider.adapter.ts
+++ src/app/adapters/watch-provider.adapter.ts
@@ -15,48 +15,63 @@ export class WatchProvidersAdapter implements Adapter<WatchProvider[]> {
     const results = item.results;
     const adaptedWatchProviders: WatchProvider[] = [];
 
-    (Object.keys(results) as (keyof typeof results)[]).forEach(region => {
-      const watchProviderTypes = results[region];
-      (Object.keys(watchProviderTypes) as (keyof typeof watchProviderTypes)[]).forEach(type => {
-        if (typeof watchProviderTypes[type] === "string") {
-          return;
-        }
-
-        const tmdbWatchProviders: TmdbWatchProvider[] = watchProviderTypes[type as WatchProviderType];
-        tmdbWatchProviders.forEach(watchProvider => {
-          let adaptedWatchProvider = adaptedWatchProviders.find(
-            includedWatchProvider => includedWatchProvider.providerName === watchProvider.provider_name
-              && includedWatchProvider.country === region
-          );
-          if (!adaptedWatchProvider) {
-            adaptedWatchProvider = new WatchProvider(
-              watchProvider.provider_id,
-              watchProvider.provider_name,
-              false,
-              false,
-              false,
-              this.imagePathProvider.getLogoUrl(watchProvider.logo_path) || "", //TODO: backup image
-              region,
-              watchProvider.display_priority
-            );
-            adaptedWatchProviders.push(adaptedWatchProvider);
-          }
-
-          switch (type) {
-            case "buy":
-              adaptedWatchProvider.isAvailableToBuy = true;
-              break;
-            case "flatrate":
-              adaptedWatchProvider.isAvailableInFlatrate = true;
-              break;
-            case "rent":
-              adaptedWatchProvider.isAvailableForRent = true;
-              break;
-          }
-        });
-      });
-    });
+    (Object.keys(results) as (keyof typeof results)[])
+      .forEach(region => this.mapRegion(region, results[region], adaptedWatchProviders));
 
     return adaptedWatchProviders;
+  }
+
+  private mapRegion(region: string, watchProviderTypes: TmdbWatchProviderInCountry, adaptedWatchProviders: WatchProvider[]) {
+    (Object.keys(watchProviderTypes) as (keyof typeof watchProviderTypes)[]).forEach(type => {
+      if (typeof watchProviderTypes[type] === "string") {
+        return;
+      }
+
+      const watchProviderType = type as WatchProviderType;
+      watchProviderTypes[watchProviderType].forEach(watchProvider =>
+        this.mapWatchProvider(
+          watchProvider,
+          watchProviderType,
+          region,
+          adaptedWatchProviders
+        )
+      );
+    });
+  }
+
+  private mapWatchProvider(watchProvider: TmdbWatchProvider, type: WatchProviderType, region: string, adaptedWatchProviders: WatchProvider[]) {
+    let adaptedWatchProvider = adaptedWatchProviders.find(
+      includedWatchProvider => includedWatchProvider.providerName === watchProvider.provider_name
+        && includedWatchProvider.country === region
+    );
+    if (!adaptedWatchProvider) {
+      adaptedWatchProvider = new WatchProvider(
+        watchProvider.provider_id,
+        watchProvider.provider_name,
+        false,
+        false,
+        false,
+        this.imagePathProvider.getLogoUrl(watchProvider.logo_path) || "", //TODO: backup image
+        region,
+        watchProvider.display_priority
+      );
+      adaptedWatchProviders.push(adaptedWatchProvider);
+    }
+
+    this.mapAvailabilityType(type, adaptedWatchProvider);
+  }
+
+  private mapAvailabilityType(type: WatchProviderType, adaptedWatchProvider: WatchProvider) {
+    switch (type) {
+      case "buy":
+        adaptedWatchProvider.isAvailableToBuy = true;
+        break;
+      case "flatrate":
+        adaptedWatchProvider.isAvailableInFlatrate = true;
+        break;
+      case "rent":
+        adaptedWatchProvider.isAvailableForRent = true;
+        break;
+    }
   }
 }
